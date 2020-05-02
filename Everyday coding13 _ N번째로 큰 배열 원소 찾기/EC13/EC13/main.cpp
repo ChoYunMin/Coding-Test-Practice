635e526242eca941ca8cd20498e1767acb5d9739
조윤민 <dbsals0618@naver.com>
2020-05-02 14:10:02 +0900

    EC13

--- Everyday coding13 _ N번째로 큰 배열 원소 찾기/EC13/EC13/main.cpp
+++ Everyday coding13 _ N번째로 큰 배열 원소 찾기/EC13/EC13/main.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
 	vector<int> inputArr;
 	cout << "Input an integer array: ";
 
-	do {
+	do { // 원하는 만큼 배열 입력받기
 		int temp;
 		cin >> temp;
 		inputArr.push_back(temp);
 	} while (getc(stdin) == ' ');
 
 	int N;
-	cout << "Input an integer N: ";
+	cout << "Input an integer N: "; 
 	cin >> N;
 	if (N > inputArr.size()) {
 		cout << "You wrote a wrong number. Please try again." << endl;
@@ -21,7 +22,8 @@ int main() {
 		return 0;
 	}
 
-	/////
+	sort(inputArr.begin(), inputArr.end()); // 정렬 (오름차순 기본)
+	cout << "Output: " << inputArr[inputArr.size() - N] << endl;
 
 	system("pause");
 	return 0;
