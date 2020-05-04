f69bb8ba81f9dd8bbd0bbefa0087d68cd600f9fc
조윤민 <dbsals0618@naver.com>
2020-05-05 01:57:36 +0900

    EC15

--- Everyday coding15 _ 링크드리스트 N번째 노드 제거하기/EC15/EC15/main.cpp
+++ Everyday coding15 _ 링크드리스트 N번째 노드 제거하기/EC15/EC15/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-class List {
+class List { // 링크드리스트 class 선언
 private: 
 	int node;
 	List* next;
@@ -9,9 +9,7 @@ public:
 	List() {
 		next = NULL;
 	}
-	~List() {
-		delete next;
-	}
+	~List() {}
 
 	void setNode(int num) {
 		node = num;
@@ -33,23 +31,65 @@ public:
 int main() {
 	cout << "Input linked list numbers: ";
 
-	List* head;
-	List* listPtr;
-	head = listPtr;
-	do {
+	List* head = new List();
+	List* listPtr = NULL;
+	int listCount = 0;
+	do { // 원하는 만큼 입력받기
 		int temp;
 		cin >> temp;
 		
-		List list;
-		list.setNode(temp);
-		listPtr->setNext(&list);
-		listPtr = listPtr->getNext();
+		List* list = new List();
+		list->setNode(temp);
+		listCount++;
 
+		if (listCount == 1) {
+			head->setNext(list);
+			listPtr = list;
+		}
+		else {
+			listPtr->setNext(list);
+			listPtr = list;
+		}
 	} while (getc(stdin) == ' ');
 
-	cout << "Input N: ";
 	int Nth;
-	cin >> Nth;
+	while (true) { // 범위에 맞는 N이 입력될 때 까지 반복
+		cout << "Input N: ";
+		cin >> Nth;
+		if (Nth > listCount) {
+			cout << "N must be smaller than list size. Please try again." << endl;
+		}
+		else if (Nth <= 0) {
+			cout << "N must be bigger than 0. Please try again." << endl;
+		}
+		else
+			break;
+	}
+	
+	int inverseNth = listCount + 1 - Nth; // 앞에서 몇 번째로 바꿔주기
+	listPtr = head;
+	for (int i = 1; i <= listCount; i++) {
+		if (inverseNth == i) {
+			List* tempPtr;
+			tempPtr = listPtr->getNext(); // tempPtr: 지우고자 하는 노드, listPtr: 지우고자 하는 것 바로 전 노드
+			if (tempPtr->getNext() != NULL) {
+				listPtr->setNext(listPtr->getNext()->getNext());
+				delete tempPtr;
+			}
+			else {
+				listPtr->setNext(NULL);
+				delete tempPtr;
+			}
+		}
+		else {
+			cout << listPtr->getNext()->getNode() << " ";
+			listPtr = listPtr->getNext();
+		}
+	}
+	if (listCount == 1) { // list가 1개였을 경우 남아있는 것이 없으므로
+		cout << "null";
+	}
+	cout << endl;
 
 	system("pause");
 	return 0;
