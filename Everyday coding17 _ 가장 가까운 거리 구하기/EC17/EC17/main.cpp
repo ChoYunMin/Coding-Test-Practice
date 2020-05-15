08d518e25e219845555c27d5540c36b4a91b0364
조윤민 <dbsals0618@naver.com>
2020-05-16 01:01:47 +0900

    EC17 not completed

--- Everyday coding17 _ 가장 가까운 거리 구하기/EC17/EC17/main.cpp
+++ Everyday coding17 _ 가장 가까운 거리 구하기/EC17/EC17/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
 int main() {
@@ -9,10 +10,44 @@ int main() {
 	cout << "Input col number: ";
 	cin >> colN;
 
+	srand(time(NULL));
 	int** arrMap = new int*[rowN];
 	for (int i = 0; i < rowN; i++) {
 		arrMap[i] = new int[colN];
+		for (int j = 0; j < colN; j++) {
+			arrMap[i][j] = rand() % 2;
+		}
 	}
+
+	for (int i = 0; i < rowN; i++) {
+		for (int j = 0; j < colN; j++) {
+			cout << arrMap[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	while (true) {
+		cout << "Start row col number: ";
+		int startX, startY;
+		cin >> startY >> startX;
+		if (arrMap[startY][startX] != 1) {
+			cout << "You can choose only 1 position." << endl;
+		}
+		else
+			break;
+	}
+	while (true) {
+		cout << "Finish row col number: ";
+		int finishX, finishY;
+		cin >> finishY >> finishX;
+		if (arrMap[finishY][finishX] != 1) {
+			cout << "You can choose only 1 position.: " << endl;
+		}
+		else
+			break;
+	}
+ 	
+
 
 	system("pause");
 	return 0;
