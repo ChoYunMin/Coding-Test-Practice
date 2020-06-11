3c95e56ac243ace79b2d9149657e9a2bd65d5347
조윤민 <dbsals0618@naver.com>
2020-06-11 16:32:23 +0900

    ec20

---
+++ Everyday coding20 _ 배열 인덱스 처음으로 돌아오기/EC20/EC20/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	cout << "Input array: ";
+	do {
+
+	} while (getc(stdin) == ' ');
+
+
+	system("pause");
+	return 0;
+}
